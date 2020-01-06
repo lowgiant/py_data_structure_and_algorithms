cdc17357eb21272809f7ac192ba8798d5508a793
hwang si yeon <lowgiant@gmail.com>
2020-01-06 22:18:44 +0900

    [생성] 난수 코드 작성

---
+++ 01-Data_structure/01-06-random/01-06-random.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+import random
+
+values = [1,2,3,4,5,6,7,8,9,10]
+
+random.shuffle(values)
+print("값 섞기 : ", values)
+
+ten = random.randint(0,10)
+
+print("0~10, 정수 섞기 : ", ten)
+
