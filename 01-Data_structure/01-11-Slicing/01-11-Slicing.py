6f2b8add988d288d997a8a0373653c3c459ace8f
hwang si yeon <lowgiant@gmail.com>
2020-01-11 23:10:20 +0900

    [생성] 슬라이싱 코드 작성

---
+++ 01-Data_structure/01-11-Slicing/01-11-Slicing.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*- 
+
+word = "안녕하세요."
+
+print("시작부터 끝까지 :", word[:] )
+print("첫 단어만 : ", word[0])
+print("두 단어만 : ", word[:2])
+print("맨 마지막 단어 : ", word[-1])
+print("스탭 : ", word[0:2:1])
