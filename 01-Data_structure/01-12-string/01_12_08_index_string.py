d1c4511d152476379e79f96e011fea27cc2decdd
hwang si yeon <lowgiant@gmail.com>
2020-01-18 22:44:43 +0900

    [생성] index, find 코드 작성

---
+++ 01-Data_structure/01-12-string/01_12_08_index_string.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*- 
+
+sample = "Hi my name is jiny"
+sample_find = sample.find("j")
+print("find 글자 찾기 : ", sample_find)
+
+sample_index = sample.index("j")
+print("index 글자 찾기 : ", sample_index)
+
+sample_rindex = sample.rindex("j")
+print("rindex 글자 찾기 : ", sample_rindex)
+
+sample_rfind = sample.rfind("k")
+print("rindex 글자 찾기 : ", sample_rfind)
