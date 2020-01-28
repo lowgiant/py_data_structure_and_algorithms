5a542ba139b32338c4c5e764f69326b8a02f1a7f
hwang si yeon <lowgiant@gmail.com>
2020-01-28 21:26:19 +0900

    [생성] 리스트 extend 코드 작성

---
+++ 01-Data_structure/01-14_list/01_14_02_extend_list.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*- 
+
+people = ["jon", "jin"]
+print("사람이름 : ", people)
+people.extend("jiny")
+
+print("exend 추가된 내용 확인 : ", people)
+people += ["jiny"]
+print("추가된 내용 확인 : ", people)
