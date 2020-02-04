c8069923c4cc834909433d931e5da91738cac9b3
hwang si yeon <lowgiant@gmail.com>
2020-02-04 23:26:01 +0900

    [생성] 리스트 정렬 코드 작성

---
+++ 01-Data_structure/01-14_list/01_14_09_sort_list.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- 
+
+people = ["jon", "jin","jon","apple"]
+people.sort()
+print("리스트 오름차순 정렬 확인 : ", people)
+people.sort(reverse = True)
+print("리스트 내림차순 정렬 확인 : ", people)
+
