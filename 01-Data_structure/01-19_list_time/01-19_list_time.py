2085238a93d8cde1c98891f6b9104ac07cf9f59d
hwang si yeon <lowgiant@gmail.com>
2020-02-15 21:35:18 +0900

    [추가] 리스트 성능 분석 코드 추가

--- 01-Data_structure/01-19_list_time/01-19_list_time.py
+++ 01-Data_structure/01-19_list_time/01-19_list_time.py
@@ -12,8 +12,17 @@ def test2():
     for i in range(1000):
         l.append(i)
 
-def tes3():
+def test3():
+    l = [i for i in range(1000)]
+
+def test4():
     l = list(range(1000))
 
-t1 = timeit.Timer()
-print("concat ", t1.timeit(number=1000))    
+t1 = timeit.Timer("test1()", "from __main__ import test1")
+print("concat ", t1.timeit(number=1000), "milliseconds")
+t2 = timeit.Timer("test2()", "from __main__ import test2")
+print("append ", t2.timeit(number=1000), "milliseconds")
+t3 = timeit.Timer("test3()", "from __main__ import test3")
+print("comprehension ", t3.timeit(number=1000), "milliseconds")
+t4 = timeit.Timer("test4()", "from __main__ import test4")
+print("list range ", t4.timeit(number=1000), "milliseconds")
