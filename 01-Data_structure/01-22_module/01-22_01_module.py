989b0678caf59ee0bf8a16bd7752c1a3d4a9f27c
hwang si yeon <lowgiant@gmail.com>
2020-03-13 22:45:43 +0900

    [생성] module 코드 작성

---
+++ 01-Data_structure/01-22_module/01-22_01_module.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*- 
+
+def append(number, number_list=[]):
+    number_list.append(number)
+    return number_list
+
+print("리스트 5 추가:", append(5))
+print("리스트 5 추가:",append(7))
+print("리스트 5 추가:",append(2))
