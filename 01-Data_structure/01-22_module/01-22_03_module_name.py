44249c3a47ccc79f2f97344e586faf84ca178f4e
hwang si yeon <lowgiant@gmail.com>
2020-03-15 22:37:21 +0900

    [생성] module name 코드 작성

---
+++ 01-Data_structure/01-22_module/01-22_03_module_name.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- 
+
+hello = "hello"
+
+def word():
+    return "world"
+
+if __name__ == "__main__":
+    print("{0} 직접실행됨".format(__name__))
+else:
+    print("{0} 임포트됨".format(__name__))
