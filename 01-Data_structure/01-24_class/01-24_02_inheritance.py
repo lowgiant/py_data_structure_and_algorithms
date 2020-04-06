55e8acb5911154ac9916228b3a33db08db364cbf
hwang si yeon <lowgiant@gmail.com>
2020-04-06 23:25:33 +0900

    [코드] inheritance 코드 작성

---
+++ 01-Data_structure/01-24_class/01-24_02_inheritance.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+
+class SampleClass(object):
+    pass
+
+class OuterClass(object):
+    class InnerClass(object):
+        pass
+
+class ChildClass(ParentClass):
+    """부모 클래스 상속"""
+    
